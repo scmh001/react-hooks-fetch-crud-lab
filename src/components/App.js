90c552bdb15a361840b5b4a7b93175766d691749
Shukri Hussein <husseinshukri@outlook.com>
2024-03-21 21:37:34 -0400

    lab-complete

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,20 @@ import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+  function handleQuestionAdded(question) {
+    setQuestions((prevQuestions) => [...prevQuestions, question]);
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? (
+        <QuestionForm onQuestionAdded={handleQuestionAdded} />
+      ) : (
+        <QuestionList questions={questions} />
+      )}
     </main>
   );
 }
